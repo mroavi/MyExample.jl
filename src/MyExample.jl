44a7a7907864b1fdda4241ebc7b6267b6afc22a6
Martin Roa Villescas <m.roa.villescas@tue.nl>
2020-03-10 13:17:41 +0100

    Add the my_f function
    my_f is a linear function just for demonstration

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,12 @@
 module MyExample
 
+# Note that if this package is imported using the "import" keyword, the exports
+# declared inside this will not take effect. On the other hand, if the keyword
+# "using" is used, then they will take effect.
+export my_f
+
 greet() = print("Hello World!")
+
+include("extra_file.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using MyExample
 using Test
 
+# "import" is essentially just like "using" except that it forces the "export"
+# statements to be ignored.
+# import MyExample
+
 @testset "MyExample.jl" begin
-    # Write your own tests here.
+    # 2x + 3y
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
 end
